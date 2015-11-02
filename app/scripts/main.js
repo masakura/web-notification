93978e52f116a850c2d7e55e51541da268bcca1a
Tomo Masakura <fj0706kr@aa.jp.fujitsu.com>
2015-11-02 17:58:22 +0900

    バイブレーショん

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,7 +11,11 @@
   $(document).on('click', '#notify', function () {
     Notification.requestPermission(function (result) {
       console.assert(result === 'granted', result);
-      registration.showNotification('Hogehoge');
+      registration.showNotification('Hogehoge', {
+        body: 'Buzz!',
+        vibrate: [200, 100, 200, 100, 200, 100, 200],
+        tag: 'vibration-sample'
+      });
     });
   });
 })();
